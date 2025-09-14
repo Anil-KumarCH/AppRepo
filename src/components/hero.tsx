dbc5d369434dd3116e867f3b47eca7dfb52691e6
AnilKumar <anilsa7989@gmail.com>
2025-09-14 12:34:55 +0530

    Fixed layout issue in hero sections in globals.css and hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -64,7 +64,7 @@ export default function Hero() {
             transition={{ duration: 2 }}
             className="hero-text"
           >
-            Anil Kumar Choppadandi, Cloud Engineer @ TCS
+            Anil Kumar Choppadandi, CloudEngineer @ TCS
           </motion.h1>
         )}
       </AnimatePresence>
